50875bf48b9d2ecc381704b2cc9817d3778411cf
Sam Casto <9531788+SamCasto@users.noreply.github.com>
2025-03-06 19:04:23 -0500

    Add files via upload

---
+++ leagueScraper/src/leagueScraper/JUnit.java
@@ -0,0 +1,14 @@
+package leagueScraper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JUnit {
+
+	@Test
+	void testPath() {
+		fail("Not yet implemented");
+	}
+
+}
